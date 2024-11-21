549270ee9d8a57c78e8c27b8425458e5d61c9428
Khaiqol <Khaiqol@gmail.com>
2024-11-21 21:02:58 +0700

    memebuat object jenis kelamin

--- app/src/main/java/com/example/questnavigasimvvm_115/model/JenisKelamin.kt
+++ app/src/main/java/com/example/questnavigasimvvm_115/model/JenisKelamin.kt
@@ -1,2 +1,12 @@
 package com.example.questnavigasimvvm_115.model
 
+import com.example.questnavigasimvvm_115.R
+
+data class JenisKelaminItem(val stringResId: Int)
+
+object JenisKelamin {
+    val jenisK = listOf(
+        JenisKelaminItem(R.string.laki),
+        JenisKelaminItem(R.string.wanita)
+    )
+}
