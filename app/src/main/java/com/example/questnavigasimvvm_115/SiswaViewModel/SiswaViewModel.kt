cb3e9ba59d77884d3af158973685d5f7b695bd3d
Khaiqol <Khaiqol@gmail.com>
2024-11-21 21:05:01 +0700

    membuat fun di class view model

--- app/src/main/java/com/example/questnavigasimvvm_115/SiswaViewModel/SiswaViewModel.kt
+++ app/src/main/java/com/example/questnavigasimvvm_115/SiswaViewModel/SiswaViewModel.kt
@@ -1,3 +1,29 @@
 package com.example.questnavigasimvvm_115.SiswaViewModel
 
-data class SiswaViewModel()
+import androidx.lifecycle.ViewModel
+import com.example.questnavigasimvvm_115.model.DataSiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+
+    //Request
+    private val _statusUI = MutableStateFlow(DataSiswa())
+
+    //Response
+    val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()
+
+    fun setDataSiswa(ls: MutableList<String>) {
+        _statusUI.update {statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                gender = ls[1],
+                alamat = ls[2],
+                email = ls[3],
+                notelepon = ls[4]
+            )
+        }
+    }
+}
