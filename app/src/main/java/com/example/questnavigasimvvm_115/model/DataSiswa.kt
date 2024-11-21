6f4de4df50ed5e07d0a1dd7cac96bb73c4a5b127
Khaiqol <Khaiqol@gmail.com>
2024-11-21 21:01:26 +0700

    pada model membuat data class siswa

--- app/src/main/java/com/example/questnavigasimvvm_115/model/DataSiswa.kt
+++ app/src/main/java/com/example/questnavigasimvvm_115/model/DataSiswa.kt
@@ -1,3 +1,9 @@
 package com.example.questnavigasimvvm_115.model
 
-data class DataSiswa()
+data class DataSiswa(
+    val nama: String ="",
+    val alamat: String ="",
+    val gender: String ="",
+    val email: String ="",
+    val notelepon: String =""
+)
